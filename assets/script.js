639b81e410e7fad9bc2ee37628dc498e93fd0763
karasu yami <demigodlinty@gmail.com>
2022-10-02 01:32:41 -0500

    added return value and the end of generatepassword function now properly working

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-// Assignment code here
+
 var generateBtn = document.querySelector("#generate");
 
-// Get references to the #generate element
+
  function wholeRandom(min, max){
   if (!max) {
     max = min
@@ -13,15 +13,16 @@ var generateBtn = document.querySelector("#generate");
  }
 
  function wholeRandomSelected(list) {
-    return list[wholeRandom(0, list.length - 1)]
+    return list[wholeRandom(list.length)]
  }
 
-
-function generatePassword(){
 //created user input referring to the pefered amount of characters the user wishes to have
+function generatePassword(){
+
   var userInput = window.prompt('between 8 and 128 characters how long would you want you password?')
 
   var passwordCompacity = parseInt(userInput)
+  
     if (isNaN(passwordCompacity)){
       window.alert('invalid!: you must use numbers only.')
       return
@@ -29,8 +30,9 @@ function generatePassword(){
      //the window alert lets the user know that their password are either too short or too 128
     if (passwordCompacity < 8 || passwordCompacity > 128) {
       window.alert('ERROR: passowrd length is not between 8 and 128 characters')
+      return
     } 
-//assigned variables to windows used to give users options of characters in their passowrd this gives the user a selection of choices 
+ 
     var numberChosen = window.confirm('are numbers your choice of characters within your password?')
     var lowercaseChosen = window.confirm('are lowercase your choice of characters within your password?')
     var symbolChosen = window.confirm('are symbols your choice of characters within your password?')
@@ -38,14 +40,14 @@ function generatePassword(){
     
     var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     var symbols = ["!","@","#","$","%","^","&","*","(",")",":",">","<","?","}","|","*","-","+"]
-    var upperCase = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var upperCase = []
     var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
     var randomCharacters = []
     
-    for (var i = 0; i < lowerCase.length;i++){
-      upperCase[i] = lowerCase[i].toUpperCase()
-    }
+  for (var i = 0; i < lowerCase.length; i++){
+    upperCase[i] = lowerCase[i].toUpperCase()
+  }
 
   if (uppercaseChosen === true){
     randomCharacters.push(upperCase)
@@ -66,16 +68,20 @@ function generatePassword(){
     randomCharacters.push(lowerCase)
     
   }
+   else if(randomCharacters.length === 0){
+    randomCharacters.push(symbols)
+  }
   
   var finalProduct = ""
 
-  for (var i = 0; i < passwordCompacity;i++){
+  for (var i = 0; i < passwordCompacity; i++) {
     var charSelected = wholeRandomSelected(randomCharacters)
     var randomEl = wholeRandomSelected(charSelected)
     finalProduct += randomEl
-    console.log(randomEl)
+    
   } 
-  
+  console.log(finalProduct)
+  return finalProduct
 }
 // Write password to the #password input
 function writePassword() {
