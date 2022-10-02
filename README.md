dfba1c4afa0b3b2170c60a65239a02db0abd7e02
karasu yami <demigodlinty@gmail.com>
2022-10-02 08:36:34 -0500

    all of the code is commented on and structured no bugs readme file is completed as well

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # password-creator
+
+## Description
+The password creator is a webpage that generates a password for you.
+how does this is a interesting process all with javascript numbers symbol uppercase and lowercase letters.It takes all of the characters selected and gives a random sequence of characters.If you need a good password that will keep your personal information protected use the password generator!.I hope all new coming users enjoy the webpage and understand the process it takes to make a password generator. 
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+n/a
+
+## Usage
+The password generator is a simple proccess when it comes to its usage.
+step one click on the generate button you will be greeted by a window prompt.On the window prompt click you are ask between 8 and 128 characters. type in the number of characters you want then select "ok" if you were to type in symbols and letters or press "cancel" you will get an alert that reminds you to use the correct characters and press ok it isnt a error more so a reminder to make sure you're going through the process correctly. After you press ok yo will be greeted with 4 confirm windows press ok to select your choice of characters and cancel to dispose of the unwanted characters.Then the magic will work and you have a brand new password!! last but not least enjoy your lovley password.
+
+## Credits
+
+N/A
+
+## License
+
+MIT License
+
+Copyright (c) 2022 ElijahLinton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't necessary, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 
 var generateBtn = document.querySelector("#generate");
 
-
+//function set in place to select random index between list specifically for this case arrays for choice of characters
  function wholeRandom(min, max){
   if (!max) {
     max = min
@@ -11,44 +11,47 @@ var generateBtn = document.querySelector("#generate");
   var randomize = Math.random()
   return Math.floor(min*(1 - randomize) + randomize*max)
  }
-
+//wholeRandomSelected function returns list of random array indexs ranging from numbers symbols and letters
  function wholeRandomSelected(list) {
     return list[wholeRandom(list.length)]
  }
 
 //created user input referring to the pefered amount of characters the user wishes to have
 function generatePassword(){
-
+// user input give user the opportunity to choose between 8 and 128 characters which number symbol or letters the user wishes
   var userInput = window.prompt('between 8 and 128 characters how long would you want you password?')
-
+// passwordCompacity turns whatever string number into an actual integer  to help interpret the length of password
   var passwordCompacity = parseInt(userInput)
-  
+  //these if conditions reminds the user to make sure they arer using the correct characters and choosing the right option to proceed
     if (isNaN(passwordCompacity)){
-      window.alert('invalid!: you must use numbers only.')
+      window.alert("REMINDER!: make sure you're using numbers only to specify your peferred length and press 'ok' afterwards.")
       return
     }
      //the window alert lets the user know that their password are either too short or too 128
     if (passwordCompacity < 8 || passwordCompacity > 128) {
-      window.alert('ERROR: passowrd length is not between 8 and 128 characters')
+      window.alert('REMINDER!: make sure your passowrd length is between 8 and 128 characters.')
       return
     } 
- 
+
+    
+ // these variables are choices the user has they ccan decide what do they want and what they dont want in their password
     var numberChosen = window.confirm('are numbers your choice of characters within your password?')
     var lowercaseChosen = window.confirm('are lowercase your choice of characters within your password?')
     var symbolChosen = window.confirm('are symbols your choice of characters within your password?')
     var uppercaseChosen = window.confirm('are uppercase letters your choice of characters ')
-    
+    //depending on the choices the user chooses they will have choice or multiple choices of characters in their password
     var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     var symbols = ["!","@","#","$","%","^","&","*","(",")",":",">","<","?","}","|","*","-","+"]
     var upperCase = []
     var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-
+// random characters is a place holder array for characters that the user selected
     var randomCharacters = []
-    
+    //an array of uppercase characters definitley makes sense but seemed more simple to create a loop that turns lowercase letters to uppercase upon command
   for (var i = 0; i < lowerCase.length; i++){
     upperCase[i] = lowerCase[i].toUpperCase()
   }
-
+//this is where the randomCharacters empty array is most important
+//it is implemented inside the if else conditions to say if a certain character array or multiple is selected it will go inside the empty array
   if (uppercaseChosen === true){
     randomCharacters.push(upperCase)
     
@@ -71,9 +74,10 @@ function generatePassword(){
    else if(randomCharacters.length === 0){
     randomCharacters.push(symbols)
   }
-  
+  // the final product variable is self explanatory it is the key to the most important par of the webpage
+  // the actual password will be string instead of listed indexed characters
   var finalProduct = ""
-
+//the loop collects the amount of characters needed to complete the password compacity then returns the value directly into the text area
   for (var i = 0; i < passwordCompacity; i++) {
     var charSelected = wholeRandomSelected(randomCharacters)
     var randomEl = wholeRandomSelected(charSelected)
